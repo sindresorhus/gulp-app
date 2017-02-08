f5fe9546e1c892322aacbd2f3e7435b6f2cdadd4
zeakd <stuzeakd@kaist.ac.kr>
2017-02-08 17:56:47 +0900

    Simple error logger

--- app/index.js
+++ app/index.js
@@ -213,7 +213,9 @@ function updateTray() {
 app.on('ready', () => {
 	const name = process.platform === 'darwin' ? 'tray-icon-Template' : 'tray-icon-alt';
 	tray = new Tray(path.join(__dirname, `${name}.png`));
-
+	notifi();
 	createTrayMenu();
 	updateTray();
 });
+
+process.on('uncaughtException', (e) => { console.error(e) });
